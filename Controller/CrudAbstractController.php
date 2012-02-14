2898f4a2bf478a7febda1ba4ffd54c3934eb120f
hlecorche <hlecorche@e-commit.fr>
2012-02-14 15:42:17 +0100

    Correction copyright

--- Controller/CrudAbstractController.php
+++ Controller/CrudAbstractController.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the EcommitCrudBundle package.
+ *
+ * (c) E-commit <contact@e-commit.fr>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Ecommit\CrudBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
--- DoctrineExtension/CountSqlWalker.php
+++ DoctrineExtension/CountSqlWalker.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the EcommitCrudBundle package.
  *
- * (c) Hubert LECORCHE <hlecorche@e-commit.fr>
+ * (c) E-commit <contact@e-commit.fr>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
