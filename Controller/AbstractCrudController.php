2233d134ceaca507e59f3ccf5d710f4015d4296a
hlecorche <hlecorche@e-commit.fr>
2014-07-03 17:37:21 +0200

    Revert "Add createCrud method in AbstractCrudController" Public methods in controller classes should only be actions
    This reverts commit 667f735197c700b8fe5b6201c13134b42db603f2.

--- Controller/AbstractCrudController.php
+++ Controller/AbstractCrudController.php
@@ -11,22 +11,12 @@
 
 namespace Ecommit\CrudBundle\Controller;
 
-use Ecommit\CrudBundle\Crud\Crud;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 abstract class AbstractCrudController extends Controller
 {
     protected $cm;
-
-    /**
-     * @param $sessionName
-     * @return Crud
-     */
-    public function createCrud($sessionName)
-    {
-        return $this->get('ecommit_crud.factory')->create($sessionName);
-    }
 
     public function autoListAction()
     {
