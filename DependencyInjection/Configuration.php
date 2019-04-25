b40e06ad84993349ce698e80894b4bd94430291a
hlecorche <hlecorche@e-commit.fr>
2019-04-25 12:09:18 +0200

    [Config] Moove root node information

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -27,8 +27,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('ecommit_crud');
+        $treeBuilder = new TreeBuilder('ecommit_crud');
+        $rootNode = $treeBuilder->getRootNode();
 
         $rootNode
             ->children()
